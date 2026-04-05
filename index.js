98e618d7c5dc0bf7345220c35efad4b05b41c3ca
Bernard Mutuma Marangu <mbmutuma@gmail.com>
2026-04-05 19:47:24 +0300

    creation of findMaxim, calculateTax, convertToUpperCase, isPalindrome and calculateDiscountedPrice functions

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 
 
+function calculateTax(amount) {return amount*0.1};
 
+function convertToUpperCase(text) {return text.toUpperCase()};
 
+function findMaximum(num1, num2) {if (num1>num2){return num1} else {return num2}}
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+function isPalindrome(word) {
+  for (let i = 0; i < word.length / 2; i++) {
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {let discountedPrice = originalPrice - originalPrice*discountPercentage/100; return discountedPrice}
+
+console.log(findMaximum (4, 5))
+console.log(calculateTax (1000))
+console.log(convertToUpperCase ("bernard"))
+console.log(isPalindrome ("bread"))
+console.log(calculateDiscountedPrice(1000, 20))
